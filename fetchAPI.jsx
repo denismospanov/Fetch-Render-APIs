709030ef7948fc93ff899854682ebfe6c32418aa
denismospanov <115163705+denismospanov@users.noreply.github.com>
2023-02-25 20:19:32 -0500

    Update fetchAPI.jsx

--- fetchAPI.jsx
+++ fetchAPI.jsx
@@ -1,146 +1,58 @@
-const Pagination = ({ items, pageSize, onPageChange }) => {
-  const { Button } = ReactBootstrap;
-  if (items.length <= 1) return null;
-
-  let num = Math.ceil(items.length / pageSize);
-  let pages = range(1, num + 1);
-  const list = pages.map(page => {
-    return (
-      <Button key={page} onClick={onPageChange} className="page-item">
-        {page}
-      </Button>
-    );
-  });
-  return (
-    <nav>
-      <ul className="pagination">{list}</ul>
-    </nav>
+function App() {
+  const { Container } = ReactBootstrap;
+  const { useState, useEffect } = React;
+  const [data, setData] = useState({ meals: [] });
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [url, setUrl] = useState(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
   );
-};
-const range = (start, end) => {
-  return Array(end - start + 1)
-    .fill(0)
-    .map((item, i) => start + i);
-};
-function paginate(items, pageNumber, pageSize) {
-  const start = (pageNumber - 1) * pageSize;
-  let page = items.slice(start, start + pageSize);
-  return page;
-}
-const useDataApi = (initialUrl, initialData) => {
-  const { useState, useEffect, useReducer } = React;
-  const [url, setUrl] = useState(initialUrl);
+  const [recipeId, setRecipeId] = useState('');
 
-  const [state, dispatch] = useReducer(dataFetchReducer, {
-    isLoading: false,
-    isError: false,
-    data: initialData
-  });
+  console.log("Rendering App");
 
-  useEffect(() => {
-    let didCancel = false;
+  useEffect(() => {   // Handles the LifeCycle Events
+    console.log("Fetching data...");
     const fetchData = async () => {
-      dispatch({ type: "FETCH_INIT" });
+      setIsLoading(true);
       try {
         const result = await axios(url);
-        if (!didCancel) {
-          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-        }
+        setData(result.data);
       } catch (error) {
-        if (!didCancel) {
-          dispatch({ type: "FETCH_FAILURE" });
-        }
+        setIsError(true);
       }
+      setIsLoading(false);
     };
-    fetchData();
-    return () => {
-      didCancel = true;
-    };
-  }, [url]);
-  return [state, setUrl];
-};
-const dataFetchReducer = (state, action) => {
-  switch (action.type) {
-    case "FETCH_INIT":
-      return {
-        ...state,
-        isLoading: true,
-        isError: false
-      };
-    case "FETCH_SUCCESS":
-      return {
-        ...state,
-        isLoading: false,
-        isError: false,
-        data: action.payload
-      };
-    case "FETCH_FAILURE":
-      return {
-        ...state,
-        isLoading: false,
-        isError: true
-      };
-    default:
-      throw new Error();
-  }
-};
-// App that gets data from Hacker News url
-function App() {
-  const { Fragment, useState, useEffect, useReducer } = React;
-  const [query, setQuery] = useState("MIT");
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 10;
-  const [{ data, isLoading, isError }, doFetch] = useDataApi(
-    "https://hn.algolia.com/api/v1/search?query=MIT",
-    {
-      hits: []
-    }
-  );
-  const handlePageChange = e => {
-    setCurrentPage(Number(e.target.textContent));
-  };
-  let page = data.hits;
-  if (page.length >= 1) {
-    page = paginate(page, currentPage, pageSize);
-    console.log(`currentPage: ${currentPage}`);
-  }
-  return (
-    <Fragment>
-      <form
-        onSubmit={event => {
-          doFetch("http://hn.algolia.com/api/v1/search?query=${query}");
-          event.preventDefault();
-        }}
-      >
-        <input
-          type="text"
-          value={query}
-          onChange={event => setQuery(event.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
 
+    fetchData();
+  }, [url]);
+
+  const handleNewRecipeClick = () => {
+    setRecipeId(Math.floor(Math.random() * 1000000));
+  }
+
+  useEffect(() => {
+    if (recipeId) {
+      setUrl(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
+    }
+  }, [recipeId]);
+
+  return (
+    <Container>
       {isError && <div>Something went wrong ...</div>}
 
       {isLoading ? (
         <div>Loading ...</div>
       ) : (
-        <ul>
-          {page.map(item => (
-            <li key={item.objectID}>
-              <a href={item.url}>{item.title}</a>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h1>{data.meals[0].strMeal}</h1>
+          <img src={data.meals[0].strMealThumb} alt={data.meals[0].strMeal} />
+          <p>{data.meals[0].strInstructions}</p>
+          <button onClick={handleNewRecipeClick}>New Recipe</button>
+        </div>
       )}
-      <Pagination
-        items={data.hits}
-        pageSize={pageSize}
-        onPageChange={handlePageChange}
-      ></Pagination>
-    </Fragment>
+    </Container>
   );
 }
 
-// ========================================
 ReactDOM.render(<App />, document.getElementById("root"));
